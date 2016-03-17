89a49a0a636dbb36f69684e13ad28e85f47c63bc
Csaba Simon <csaba.simon@logmein.com>
2016-03-17 11:47:46 +0100

    remove demo from urls

--- server/index.js
+++ server/index.js
@@ -92,7 +92,7 @@ var proxy = require('./proxy');
 app.use('/api/proxy', parsers, proxy({
   whitelist: [
     'http://www.airnowapi.org/aq/data',
-    'https://timeseries.demo.xively.com/api/v4/data/xi/blue/v1',
+    'https://timeseries.xively.com/api/v4/data/xi/blue/v1',
   ],
 }));
 
